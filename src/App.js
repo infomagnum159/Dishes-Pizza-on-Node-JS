e055be8cc9be0bfecdb748027c9411f9dd57c38f
infomagnum159 <infomagnum159@gmail.com>
2022-08-06 04:33:55 +0600

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation';
-import Orders from './components/Orders/Orders';
-import Dishes from './components/Dishes/Dishes';
+import Orders from './container/Orders/Orders';
+import Dishes from './container/Dishes/Dishes';
 import AddPizza from './components/AddPizza/AddPizza';
-import EditPizza from './EditPizza/EditPizza';
+import EditPizza from './components/EditPizza/EditPizza';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Router>
         <Navigation />
         <Route exact path='/' component={Dishes} />
         <Route path='/orders' component={Orders} />
         <Route path='/add-dishes' component={AddPizza} />
         <Route path='/:id/edit' component={EditPizza} />
-      </Router>
     </div>
   );
 }
